5915f136f15167e4b88029974b096d4677b4c5c9
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-10-03 23:12:21 +0200

    Switch to Hardhat, rewrite tests to Typescript

--- run-tests.js
+++
@@ -1,7 +0,0 @@
-const { spawn } = require("child_process");
-
-spawn("ganache", ["--mnemonic", "horn horn horn horn horn horn horn horn horn horn horn horn"]);
-
-const truffleArgs = ["test", "--migrations_directory", "test"];
-if (process.argv[2] !== undefined) truffleArgs.push(process.argv[2]);
-setTimeout(() => spawn("truffle", truffleArgs, { stdio: "inherit" }).on("exit", () => process.exit()), 4200);
---
+++ scripts/deploy-feecollector.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat";
+
+// Constructor arguments
+const guildFeeCollector = "0x..."; // The address that will receive Guild's share from the funds.
+const guildSharex100 = 0; // The percentage of Guild's share multiplied by 100 (e.g 500 for a 5% cut).
+const poapFeeCollector = "0x..."; // The address that will receive POAP's share from the funds.
+const poapSharex100 = 0; // The percentage of POAP's share multiplied by 100 (e.g 500 for a 5% cut).
+
+async function main() {
+  const FeeCollector = await ethers.getContractFactory("FeeCollector");
+  const feeCollector = await FeeCollector.deploy(guildFeeCollector, guildSharex100, poapFeeCollector, poapSharex100);
+
+  console.log(
+    `Deploying contract to ${
+      ethers.provider.network.name !== "unknown" ? ethers.provider.network.name : ethers.provider.network.chainId
+    }...`
+  );
+
+  await feeCollector.deployed();
+
+  console.log("FeeCollector deployed to:", feeCollector.address);
+  console.log("Constructor arguments:", guildFeeCollector, guildSharex100, poapFeeCollector, poapSharex100);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
